7ddf607b6cb69d6b7ecbcf95fcdd352d0b84f47d
Marcin Konarski <marcin.konarski@codestation.org>
2005-12-28 15:58:01 +0000

    r1065: Cleaning up comments.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /*
 ---            `funlab' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski             ---
 
-	prog.c - this file is integral part of `funlab' project.
+	main.c - this file is integral part of `funlab' project.
 
 	i.  You may not make any changes in Copyright information.
 	ii. You must attach Copyright information to any part of every copy
