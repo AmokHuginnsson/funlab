38a1f8095b5756b91bee018db6657c9bd1d6c824
Marcin Konarski <marcin.konarski@codestation.org>
2014-06-25 21:14:14 +0200

    Sync to yaal.

--- src/options.cxx
+++ src/options.cxx
@@ -41,13 +41,15 @@ using namespace yaal::tools;
 using namespace yaal::tools::util;
 
 namespace funlab {
- 
+
 namespace {
 
 bool set_variables( HString& option_, HString& value_ ) {
-	::fprintf( stdout, "option: [%s], value: [%s]\n",
-			option_.raw(), value_.raw() );
-	return ( false );
+	static bool const FUNLAB_RC_DEBUG( !! ::getenv( "FUNLAB_RC_DEBUG" ) );
+	if ( FUNLAB_RC_DEBUG ) {
+		cout << "option: [" << option_ << "], value: [" << value_ << "]" << endl;
+	}
+	return ( true );
 }
 
 void version( void* ) {
@@ -85,7 +87,7 @@ int handle_program_options( int argc_, char** argv_ ) {
 		( "help", program_options_helper::option_value( stop ), 'h', HProgramOptionsHandler::OOption::TYPE::NONE, "display this help and stop", program_options_helper::callback( util::show_help, &info ) )
 		( "dump-configuration", program_options_helper::option_value( stop ), 'W', HProgramOptionsHandler::OOption::TYPE::NONE, "dump current configuration", program_options_helper::callback( util::dump_configuration, &info ) )
 		( "version", program_options_helper::option_value( stop ), 'V', HProgramOptionsHandler::OOption::TYPE::NONE, "output version information and stop", program_options_helper::callback( version, NULL ) );
-	po.process_rc_file( "funlab", "", NULL );
+	po.process_rc_file( "funlab", "", set_variables );
 	if ( setup._logPath.is_empty() )
 		setup._logPath = "funlab.log";
 	int unknown( 0 );
