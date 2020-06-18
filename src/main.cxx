001f6baa5d3621f6e035acd8cf190690cfc11b0f
Marcin Konarski <marcin.konarski@codestation.org>
2020-06-18 11:36:36 +0200

    Sync to yaal.

--- src/main.cxx
+++ src/main.cxx
@@ -6,6 +6,7 @@
 
 #include <yaal/hcore/hlog.hxx>
 #include <yaal/tools/signals.hxx>
+#include <yaal/tools/hthreadpool.hxx>
 #include <yaal/tools/util.hxx>
 M_VCSID( "$Id: " __ID__ " $" )
 
@@ -28,6 +29,7 @@ OSetup setup;
 }
 
 int main ( int argc_, char * argv_ [ ] ) {
+	HScopeExitCall secTP( call( &HThreadPool::stop, &HThreadPool::get_instance() ) );
 	HScopeExitCall sec( call( &HSignalService::stop, &HSignalService::get_instance() ) );
 	M_PROLOG
 /* variables declarations for main loop: */
